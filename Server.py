5060d3e5ab18311581bc939f606b00f81b48cefc
Jose Morales <jamoralesl2021@gmail.com>
2023-12-10 16:12:12 -0500

    Adding CNN from scratch and pretrained results

--- Server.py
+++ Server.py
@@ -21,7 +21,7 @@ class CitiesData(Dataset):
         imagePaths = []
         for city in os.listdir(dataParentFolder):
             path = os.path.join(dataParentFolder, city)
-            imagePaths.extend([os.path.join(path, imageFile).replace("\\", "/") for imageFile in os.listdir(path)])    
+            imagePaths.extend([os.path.join(path, imageFile).replace("\\", "/") for imageFile in os.listdir(path)])           
         self.imagePaths = np.array(imagePaths)[dataIdxs]
     def __len__(self):
         return len(self.imagePaths)
@@ -36,8 +36,8 @@ class CitiesData(Dataset):
         latitude = latitude.split(".jpg")[0]
         image = Image.open(imagePath)
         if self.transform:
-            image = self.transform(image)
-        return image, city, float(longitude), float(latitude)
+            image = self.transform(image)    
+        return image, city, float(longitude), float(latitude)    
     def city_to_vector(self, city):
         output = np.zeros(10)
         for i in range(len(city)):
@@ -62,9 +62,9 @@ class CitiesData(Dataset):
             elif city == 'Seattle':
                 output[9] = 1
         return torch.tensor(output)
+
+
 # %%
-
-
 def getCitiesDataLoader(dataParentFolder: str, batchSize: int = 128, transforms = None):
     cityIdxs = [0]
     totalPoints = 0
@@ -97,7 +97,6 @@ def getCitiesDataLoader(dataParentFolder: str, batchSize: int = 128, transforms 
     testDataLoader = DataLoader(testData, batch_size=batchSize, shuffle=True)
     return trainDataLoader, validDataLoader, testDataLoader
     
-
 
 # %%
 def getBalancedCitiesDataLoader(dataParentFolder: str, augmentedParentFolder: str, batchSize: int = 128, transforms = None, balanced_size=200000):
@@ -138,7 +137,7 @@ def getBalancedCitiesDataLoader(dataParentFolder: str, augmentedParentFolder: st
                 count += 1
     trainIdxs = []
     validIdxs = []
-    testIdxs = []
+    testIdxs = []   
     for i in range(10):
         start = i * class_balanced_size
         stop = (i+1) * class_balanced_size
@@ -164,22 +163,26 @@ def getBalancedCitiesDataLoader(dataParentFolder: str, augmentedParentFolder: st
     validDataLoader = DataLoader(validData, batch_size=batchSize, shuffle=True)
     testDataLoader = DataLoader(testData, batch_size=batchSize, shuffle=True)
     return trainDataLoader, validDataLoader, testDataLoader
+
+
 # %%
-
-
 class AugmentedCitiesData(Dataset):
     def __init__(self, dataParentFolder: str, augmentedParentFolder: str, imagePaths, transform = None, batch_size=128):
         self.dataParentFolder = dataParentFolder
         self.augmentedParentFolder = augmentedParentFolder
         self.transform = transform
-        self.imagePaths = imagePaths  
+        self.imagePaths = imagePaths
     def __len__(self):
         return len(self.imagePaths)
     def __getitem__(self, idx: int):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         imagePath = self.imagePaths[idx]
-        pathSplits = imagePath.split(self.dataParentFolder)[1].split("/")
+        pathSplits = - 1
+        if len(imagePath.split(self.dataParentFolder)) >1:
+            pathSplits = imagePath.split(self.dataParentFolder)[1].split("/")
+        else:
+            pathSplits = imagePath.split(self.augmentedParentFolder)[1].split("/")
         city = pathSplits[0]
         city =self.city_to_vector(city)
         longitude, latitude = pathSplits[1].split(",")
@@ -212,9 +215,8 @@ class AugmentedCitiesData(Dataset):
             elif city == 'Seattle':
                 output[9] = 1
         return torch.tensor(output)
+
 # %%
-
-
 def data_generator(original_data_folder, augmented_data_folder, city, amount):
     
     path = os.path.join(original_data_folder, city)
@@ -222,18 +224,21 @@ def data_generator(original_data_folder, augmented_data_folder, city, amount):
     while count < amount:
         for imageFile in os.listdir(path):
             imagePath = os.path.join(path, imageFile).replace("\\", "/")
+
             # Transform image - slight rotations and 
             image = Image.open(imagePath)
+
             rand = random.randint(0,9)
-            if rand <= 2:
+            if rand <= 5:
                 rand2= random.randint(-3,3)
                 image = image.rotate(rand2)
-            elif rand <= 5:
-                image = ImageOps.grayscale(image)
             else:
                 image = image.filter(ImageFilter.GaussianBlur(1))
+
+
             #Write Image to AugmentedData
             image.save(os.path.join(os.path.join(augmented_data_folder, city), str(count) + ".0,1.0.jpg").replace("\\", "/"))
+
             count += 1
             if count >= amount:
                 break
@@ -242,7 +247,7 @@ def data_generator(original_data_folder, augmented_data_folder, city, amount):
 # Finally the values are first rescaled to [0.0, 1.0] and then normalized using mean=[0.485, 0.456, 0.406] and std=[0.229, 0.224, 0.225].
 #models.ViT_B_16_Weights.IMAGENET1K_V1
 
-model_name = "Inception_from_scratch2"
+model_name = "Inception_pretrained_balanced"
 model_image_size = 224
 
 
@@ -283,14 +288,14 @@ model = CityInception(10).to(device)
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-6)
 # %%
-batch_size = 128
+batch_size = 64
 transform = transforms.Compose([
     transforms.Resize(342),
     transforms.CenterCrop(299),
     transforms.ToTensor(),
     transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
 ])
-trainDataLoader, validDataLoader, testDataLoader = getCitiesDataLoader("/home/vislab-001/Documents/fmriMQP/data/", transforms = transform, batchSize=batch_size)
+trainDataLoader, validDataLoader, testDataLoader = getBalancedCitiesDataLoader("/home/vislab-001/Documents/fmriMQP/data/", "./AugmentedData", transforms = transform, batchSize=batch_size)
 # %%
 print(len(trainDataLoader))
 print(len(validDataLoader))
@@ -345,7 +350,7 @@ def evaluate_on_data(vit, dataloader):
 # %%
 
 
-num_epochs = 10
+num_epochs = 30
 count = 0
 valid_loss_array = np.zeros(num_epochs)
 valid_acc_array = np.zeros(num_epochs)
@@ -383,3 +388,6 @@ with open(model_name + '_valid_acc.npy', 'wb') as f:
 
 with open(model_name + '_train.npy', 'wb') as f:
     np.save(f, train_loss_array)
+
+
+print(evaluate_on_data(model, testDataLoader))
